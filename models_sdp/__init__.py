ede6b5a6615d66546e40564419b4e642206ee3b0
Xue He <1311965600@qq.com>
2026-04-28 13:25:31 +0800

    Delete models_sdp directory

--- models_sdp/__init__.py
+++
@@ -1,9 +0,0 @@
-from .vggbinaryconnect import *
-from .vgg_small import *
-from .vgg16 import *
-from .resnet_cifar import *
-
-from .resnet_1w1a import *
-from .resnet_1w32a import *
-from .alexnet import *
-from .swin_transformer import *
