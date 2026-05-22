749000db392337d6ac8c83103222300d25454400
Xue He <1311965600@qq.com>
2026-05-23 01:22:43 +0800

    Update data.py

--- data.py
+++ data.py
@@ -7,8 +7,6 @@ _DATASETS_MAIN_PATH = 'datasets'
 _dataset_path = {
     'cifar10': os.path.join(_DATASETS_MAIN_PATH, 'CIFAR10'),
     'cifar100': os.path.join(_DATASETS_MAIN_PATH, 'CIFAR100'),
-    'stl10': os.path.join(_DATASETS_MAIN_PATH, 'STL10'),
-    'mnist': os.path.join(_DATASETS_MAIN_PATH, 'MNIST'),
     'imagenet': {
         'train': os.path.join(_DATASETS_MAIN_PATH, 'imagenet/train'),
         'val': os.path.join(_DATASETS_MAIN_PATH, 'imagenet/val')
